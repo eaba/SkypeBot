b4265d891548905711a6fd7e970eb12dc911263c
CCYD <ccydcontact@gmail.com>
2016-03-21 19:08:02 -0400

    dont mind this mess

--- SkypeBot/Program.cs
+++ SkypeBot/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SKYPE4COMLib;
+using ChatterBotAPI;
 
 namespace SkypeBot
 {
@@ -10,6 +12,84 @@ namespace SkypeBot
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Started!!");
+
+            Skype skype = new Skype();
+            
+            ChatterBotFactory factory = new ChatterBotFactory();
+        
+            ChatterBot bot1 = factory.Create(ChatterBotType.CLEVERBOT);
+            ChatterBotSession bot1session = bot1.CreateSession();
+
+            
+
+            //Chat chat = skype.ActiveChats[1];
+
+            //chat.SendMessage("test3");
+
+            
+
+            List<string> allcontacts = new List<string>();
+            foreach (User user in skype.Friends)
+            {
+                allcontacts.Add(user.Handle);
+            }
+
+            allcontacts.ForEach(i => Console.WriteLine("{0}", i));
+            Console.WriteLine("\n");
+
+            Console.ReadKey();
+
+            
+
+
+            Console.WriteLine("watdo?\n1. Send Message\n2. Receive Message");
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("You have selected \"Send Message\"\n");
+
+                        Console.WriteLine("Please enter a contact: ");
+                        string contacttosend = Console.ReadLine();
+                        Console.WriteLine("Please enter message: ");
+                        string messagetosend = Console.ReadLine();
+                        skype.SendMessage(contacttosend, messagetosend);
+                        break;
+                    case 2:
+                        Console.WriteLine("You have selected \"Receive Message\"\n");
+
+                        foreach (IChatMessage msg in skype.MissedMessages)
+                        {
+                            //string handle = msg.Sender.Handle;
+                            //string message = "This is an auto response message.";
+                            //skype.SendMessage(handle, message);
+                            
+
+                            Console.WriteLine(msg.Body);
+                            msg.Seen = true;
+                            Console.ReadKey();
+
+                        }
+
+                        
+                        break;
+                        
+                }
+
+            }
+            
+           
+
+
+
+
+
+
         }
+
     }
 }
